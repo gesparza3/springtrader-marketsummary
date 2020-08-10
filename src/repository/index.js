0c29ccfdb23223e1814faeaa936797ecda9061e8
gesparza3 <esparzagrant@gmail.com>
2020-08-10 09:53:16 -0700

    Updated market-summary to use vault

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,6 +1,9 @@
+const fs = require("fs");
+const util = require("util");
 const mongoose = require("mongoose");
+const watch = require("node-watch");
 
-let connection;
+let connection, watcher;
 
 const getConnection = () => {
     if (!connection) {
@@ -10,14 +13,36 @@ const getConnection = () => {
     return connection;
 };
 
+const setConnection = async (username, password) => {
+  const newConnection = await mongoose.createConnection(
+    `mongodb://${username}:${password}@mongodb.${process.env.DATABASE_NAMESPACE}.svc.cluster.local:27017/${process.env.NODE_ENV}`
+  );
+
+  connection = newConnection;
+}
+
+const getDatabaseCredentials = async () => {
+    const data = await util.promisify(fs.readFile)(MONGODB_CREDENTIALS_FILE);
+
+    return JSON.parse(data);
+};
+
 const start = async () => {
-    if (!connection) {
-        console.log("Connecting to DB");
-        connection = await mongoose.createConnection(`mongodb://mongodb.${process.env.DATABASE_NAMESPACE}.svc.cluster.local:27017/${process.env.NODE_ENV}`);
-    }
+    const { username, password } = await getDatabaseCredentials();
+
+    await setConnection(username, password);
+
+    watcher = watch(MONGODB_CREDENTIALS_FILE, async () => {
+        const { username, password } = await getDatabaseCredentials();
+        await setConnection(username, password);
+    });
 };
 
 const stop = async () => {
+    if (watcher) {
+        watcher.close();
+    }
+
     if (connection) {
         await connection.close();
     }
